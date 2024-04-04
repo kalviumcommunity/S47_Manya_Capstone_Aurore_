8e9ea93f4b3fea2326c808d71869c42ffff14520
Manya1310 <manyajain324@gmail.com>
2024-04-04 09:13:07 +0530

    Created SignUp Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,17 @@
-import React from "react"
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
+import SignUpForm from "./Pages/SignUp";
 
 function App() {
-
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/signup" element={<SignUpForm />} />
       </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
