c507c50371142399c89596debb8a66e8b27c0f33
Manya1310 <manyajain324@gmail.com>
2024-04-04 10:03:27 +0530

    Fetched APOD data

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "./Pages/LandingPage";
 import SignUpForm from "./Pages/SignUp";
 import LoginForm from "./Pages/Login";
+import APOD from "./FetchedData/APOD";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<LoginForm />} />
+        <Route path="/apod" element={<APOD />}></Route>
       </Routes>
     </>
   );
