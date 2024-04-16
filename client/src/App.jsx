e3fa599280a4f7c312a0b608901041afa9ca9888
Manya1310 <manyajain324@gmail.com>
2024-04-16 16:38:53 +0530

    Created NavBar Component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,12 +8,14 @@ import MarsRover from "./FetchedData/MarsRover";
 import ISROLaunches from "./FetchedData/ISROLaunches";
 import Articles from "./FetchedData/Articles";
 import Blogs from "./FetchedData/Blogs";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/main" element={<HomePage />} />
         <Route path="/signup" element={<SignUpForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/apod" element={<APOD />}></Route>
